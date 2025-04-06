438437778a8f129ff6ee2d58bf740c2b773fa357
Mustafa Ozcan <mustafa_ozcan_@hotmail.com>
2025-04-06 22:27:12 +0300

    Add project files.

---
+++ Hospital.Repositories/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Repositories
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=DESKTOP-45N32KT;Database=HospitalDb;User Id=sa;Password=mustafa123;TrustServerCertificate=True");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Hospital.Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Repositories.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepositories<T> GenericRepositories<T>() where T : class;
+        void Save();
+    }
+}
